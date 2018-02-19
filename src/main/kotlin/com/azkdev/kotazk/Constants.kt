b410cbb28ab6f5c207312cdef1ab412f0f4332b9
azkdev <azkdev@outlook.com>
2018-02-19 23:55:20 +0200

    Documentation added with new constants.

--- src/main/kotlin/com/azkdev/kotazk/Constants.kt
+++ src/main/kotlin/com/azkdev/kotazk/Constants.kt
@@ -1,6 +1,27 @@
 package com.azkdev.kotazk
 
+/**
+ * Canvas width.
+ */
 val CW: Int = 600
+/**
+ * Canvas height.
+ */
 val CH: Int = 600
+/**
+ * Canvas background color.
+ */
+val BG: Int = 50
 
-val HR: Double = 50.0
+/**
+ * Frame rate.
+ */
+val FR: Float = 90F
+
+/**
+ * Hero radius.
+ * All the heroes will have ellipse form.
+ */
+val HR: Float = 50F
+
+val HT: String = "/textures/hero.png"
